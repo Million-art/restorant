56792b4abd06e478283df5d303a6719dc432f700
million-art <millionmulugeta09@gmail.com>
2023-03-26 08:22:16 +0300

    init

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+import doro from "../images/doro.jpg";
+import kitfo from "../images/kitfo.jpg";
+import tibs from "../images/tibs.jpg";
+import bozena from "../images/bozena.jpg";
+import aynet from "../images/aynet.jpg";
+
+export const MenuList = [
+  {
+    name: "doro wet",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: doro ,
+    price: 200,
+  },
+  {
+    name: "kitfo",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: kitfo,
+    price: 250,
+  },
+  {
+    name: "bozena shiro",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: bozena,
+    price: 300,
+  },
+  {
+    name: " tibs",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: tibs,
+    price: 100,
+  },
+  {
+    name: "bozena",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: bozena,
+    price: 400,
+  },
+  {
+    name: "beyaynet",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: aynet,
+    price: 500,
+  },
+];
